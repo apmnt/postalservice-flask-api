074b0150c37b872de776454ec885225a8077277d
apmnt <aapo.montin@student.oulu.fi>
2024-03-10 11:47:42 +0200

    Add routes for Mercari and Fril APIs

--- api/index.py
+++ api/index.py
@@ -1,4 +1,7 @@
+import asyncio
 from flask import Flask
+from flask import request
+import postalservice as ps
 
 app = Flask(__name__)
 
@@ -9,3 +12,20 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/mercari')
+def mercari():
+    mercari = ps.MercariService()
+    keyword = request.args.get('keyword', '')
+    size = request.args.get('size', '')
+    result = mercari.get_search_results({'keyword': keyword, 'size': size})
+    return result.to_json()
+
+@app.route('/fril')
+def fril():
+    fril = ps.FrilService()
+    keyword = request.args.get('keyword', '')
+    size = request.args.get('size', '')
+    result = asyncio.run(fril.get_search_results_async({'keyword': keyword, 'size': size}))
+    return result.to_json()
+
